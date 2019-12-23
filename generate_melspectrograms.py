90137c80d733a4ed69d3a98c4e2c952e1fddd5b8
Jean-Pierre Thach <jeanpierrethach@gmail.com>
2019-12-23 12:19:35 -0500

    Modified data_path input argument melspectrogram generation

--- generate_melspectrograms.py
+++ generate_melspectrograms.py
@@ -11,51 +11,48 @@ from utils import maybe_make_directory
 from feature_extraction import extract_mel_spectrogram
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str,
-                        default='train', choices=['train', 'valid', 'test'],
-                        help='Dataset to generate pickle of features. (default: %(default)s)')
-    parser.add_argument('--input_dir', type=str,
-                        default='./out',
-                        help='Directory path to the pickle files. (default: %(default)s)')
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--dataset', type=str,
+						required=True, choices=['train', 'valid', 'test'],
+						help='Dataset to generate pickle of features. (required) (default: %(default)s)')
+	parser.add_argument('--data_path', type=str,
+						required=True,
+						help='Directory path to the datasets. (required) (default: %(default)s)')
+	parser.add_argument('--input_dir', type=str,
+						default='./out',
+						help='Directory path to the pickle files. (default: %(default)s)')
 	parser.add_argument('--output_dir', type=str,
-                        default='./melspec',
-                        help='Directory path to the melspectrograms output. (default: %(default)s)')
-    parser.add_argument('--n_mels', type=int,
-                        default=128,
-                        help='Number of Mel bands to generate. (default: %(default)s)')
-    parser.add_argument('--fmax', type=int,
-                        default=8000,
-                        help='Highest frequency (in Hz). (default: %(default)s)')
+						default='./melspec',
+						help='Directory path to the melspectrograms output. (default: %(default)s)')
+	parser.add_argument('--n_mels', type=int,
+						default=128,
+						help='Number of Mel bands to generate. (default: %(default)s)')
+	parser.add_argument('--fmax', type=int,
+						default=8000,
+						help='Highest frequency (in Hz). (default: %(default)s)')
 	parser.add_argument('--hop_length', type=int,
-                        default=1024,
-                        help='Number of samples between successive frames. (default: %(default)s)')
+						default=1024,
+						help='Number of samples between successive frames. (default: %(default)s)')
 
-    args = parser.parse_args()
-    if args.dataset == "train":
-        path = "./nsynth-train.jsonwav.tar/nsynth-train"
-    elif args.dataset == "valid":
-        path = "./nsynth-valid.jsonwav.tar/nsynth-valid"
-    elif args.dataset == "test":
-        path = "./nsynth-test.jsonwav.tar/nsynth-test"
-    
-    maybe_make_directory(args.input_dir)
+	args = parser.parse_args()
 
-    return args, path
+	maybe_make_directory(args.input_dir)
+	maybe_make_directory(os.path.join(args.output_dir, args.dataset))
 
-args, path = parse_args()
+	return args
 
-os.chdir("/mnt/d/IFT6390_NSynth_data")
+args = parse_args()
 
+DATASET_PATH = args.data_path
 AUDIO_FILEPATH = "./audio/"
 
-DEFAULT_PATH = os.path.join(args.input.dir, f'filenames_{args.dataset}.pickle')
+FILENAMES_PICKLE_PATH = os.path.join(args.input_dir, f'filenames_{args.dataset}.pickle')
 
-with open(DEFAULT_PATH, 'wb') as f:
+with open(FILENAMES_PICKLE_PATH, 'wb') as f:
 	filenames_examples = pickle.load(f)
 
 for idx, filename in enumerate(filenames_examples):
-	mel_spectrogram = extract_mel_spectrogram(os.path.join(path, AUDIO_FILEPATH, filename + '.wav'), n_mels=args.n_mels, hop_length=args.hop_length, fmax=args.fmax)
+	mel_spectrogram = extract_mel_spectrogram(os.path.join(DATASET_PATH, AUDIO_FILEPATH, filename + '.wav'), n_mels=args.n_mels, hop_length=args.hop_length, fmax=args.fmax)
 	class_file = filename.split('_')[0]
 	plt.figure(figsize=(10,4))
 	librosa.display.specshow(librosa.power_to_db(mel_spectrogram, ref=np.max), 
@@ -64,3 +61,4 @@ for idx, filename in enumerate(filenames_examples):
 	plt.title("Mel spectrogram for " + class_file)
 	plt.savefig(os.path.join(args.output_dir, args.dataset, f'{class_file}_mel_spectro_{str(idx)}.png'), bbox_inches='tight', transparent=True, pad_inches=0)
 	plt.close()	
+print(f'Mel spectrograms saved in {args.output_dir} directory')
