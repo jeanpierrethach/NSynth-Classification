b75f09ae02ef86a1682e9daf8e3a5eb70d63031b
Jean-Pierre Thach <jeanpierrethach@gmail.com>
2019-12-23 11:19:26 -0500

    Removed unused parser arg

--- create_dataset_melspectrograms.py
+++ create_dataset_melspectrograms.py
@@ -24,19 +24,16 @@ def parse_args():
 						default='train', choices=['train', 'valid', 'test'],
 						help='Dataset to generate pickle of features. (default: %(default)s)')
 	parser.add_argument('--resize', action='store_true',
-                        help='Boolean flag activating resize of melspectrograms')
+                        help='Boolean flag activating resize of melspectrograms (default: False)')
 	parser.add_argument('--size', type=_size, nargs=2,
 						default='32,32',
                         help='Size must be height,width (default: (%(default)s))')
-	parser.add_argument('--output_dir', type=str,
-						default='./out',
-						help='Directory path to the outputs. (default: %(default)s)')
-	parser.add_argument('--max_size', type=int,
-						default=1920,
-						help='Maximum width or height of the input images. (default: %(default)s)')  
 	parser.add_argument('--input_dir', type=str,
 						default='./melspec',
 						help='Directory path to the melspectrograms. (default: %(default)s)')
+	parser.add_argument('--output_dir', type=str,
+						default='./out',
+						help='Directory path to the outputs. (default: %(default)s)')
 	args = parser.parse_args()
 
 	maybe_make_directory(args.output_dir)
