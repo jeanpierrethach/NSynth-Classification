54e67da750f9f2cd7139c0f563d2d807dec7d103
Jean-Pierre Thach <jeanpierrethach@gmail.com>
2019-12-23 11:25:33 -0500

    Modified model input argument

--- eval_model.py
+++ eval_model.py
@@ -16,21 +16,17 @@ def parse_args():
 	parser.add_argument('--input_dir', type=str,
 						default='./out',
 						help='Directory path to the npy files. (default: %(default)s)')
-	parser.add_argument('--model_dir', type=str,
-						default='./models',
-						help='Directory path to the model output. (default: %(default)s)')
 	parser.add_argument('--graph_dir', type=str,
 						default='./graphs',
 						help='Directory path to the graphs output. (default: %(default)s)')
-	parser.add_argument('--model_name', type=str,
+	parser.add_argument('--model_path', type=str,
 						required=True,
-						help='Name of the model. (required)')
+						help='Path of the model. (required)')
 	parser.add_argument('--normalize', action='store_true',
                         help='Boolean flag activating normalization of the confusion matrix')
 
 	args = parser.parse_args()
 	maybe_make_directory(args.input_dir)
-	maybe_make_directory(args.model_dir)
 	maybe_make_directory(args.graph_dir)
 	return args
 
@@ -43,7 +39,7 @@ class_names = np.array(['bass', 'brass', 'flute', 'guitar',
 X_test = np.load(os.path.join(args.input_dir, "test_spectro.npy"))
 y_test = np.load(os.path.join(args.input_dir, "test_spectro_labels.npy"))
 
-model = load_model(os.path.join(args.model_dir, args.model_name))
+model = load_model(args.model_path)
 
 scores = model.evaluate(X_test, y_test, verbose=1)
 print('Test loss:', scores[0])
@@ -54,4 +50,7 @@ y_test = np.argmax(y_test, axis=1)
 
 plot_confusion_matrix(y_test, y_preds, classes=class_names, normalize=args.normalize,
 					  title='Normalized confusion matrix for CNN')
-plt.savefig(os.path.join(args.graph_dir, f'cnn_normalized_{args.normalize}_{time.strftime("%Y%m%d-%H%M%S")}.png'))
+fig_name = f'cnn_normalized_{args.normalize}_{time.strftime("%Y%m%d-%H%M%S")}.png'
+fig_path = os.path.join(args.graph_dir, fig_name)
+plt.savefig(fig_path)
+print(f'Figured saved at {fig_path}')
